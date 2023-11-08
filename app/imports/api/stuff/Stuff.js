2f24f1b24ca489d1d1aac250a2e358881049be9f
Heinrich <no-reply@mail.heinrich.ninja>
2023-11-07 21:06:33 -1000

    new changes

--- app/imports/api/stuff/Stuff.js
+++ app/imports/api/stuff/Stuff.js
@@ -12,8 +12,14 @@ class StuffsCollection {
     this.collection = new Mongo.Collection(this.name);
     // Define the structure of each document in the collection.
     this.schema = new SimpleSchema({
-      name: String,
-      quantity: Number,
+      name: {
+        type: String,
+        optional: true,
+      },
+      quantity: {
+        type: Number,
+        optional: true,
+      },
       owner: String,
       status: {
         type: String,
