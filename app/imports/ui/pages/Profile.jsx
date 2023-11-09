f3c13b3ca764e9ea1208db2044339916fa517e52
Bibiana Garcia <99704806+bibiygarcia@users.noreply.github.com>
2023-11-09 00:03:43 -1000

    added some stuff to Profile

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -4,6 +4,10 @@ import { useTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import { Container } from 'react-bootstrap';
 
+/*
+Consider looking at this for account stuff: https://docs.meteor.com/api/accounts.html#Meteor-users
+ */
+
 const Profile = () => {
   // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
   const { currentUser } = useTracker(() => ({
@@ -16,7 +20,16 @@ const Profile = () => {
       Everyone can see me!
 
       {currentUser ? ([
-        <div>Only Users can see me.</div>,
+        <div>
+          Only Users can see me.
+          <br />
+          This is my username {Meteor.user().username}. This is my other stuff:
+          <ul>
+            <li>{Meteor.user()._id}</li>
+            <li>{Meteor.user().username}</li>
+            <li>{Meteor.user().emails[0].address}</li>
+          </ul>
+        </div>,
       ]) : ''}
 
       {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
