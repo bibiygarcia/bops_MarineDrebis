3c12cdb34a62c52488a278441dabf3465345ced6
Bibiana Garcia <99704806+bibiygarcia@users.noreply.github.com>
2023-11-13 08:11:04 -1000

    quickUpdates

--- app/imports/ui/pages/LandingOrganization.jsx
+++ app/imports/ui/pages/LandingOrganization.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import UnderConstruction from '../components/UnderConstruction';
+import DebrisMap from './DebrisMap';
 
 const LandingOrganization = () => (
   <div className="d-flex flex-column min-vh-100">
+    <DebrisMap />
     <UnderConstruction />
   </div>
 );
