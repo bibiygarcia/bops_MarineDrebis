ae501763c18d3445d1458a42e59151000f4239c8
Princess Rose Balasico <balasico@hawaii.edu>
2023-11-15 13:31:36 -1000

    ....

--- app/imports/ui/components/ProfileItem.jsx
+++ app/imports/ui/components/ProfileItem.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Col, Container } from 'react-bootstrap';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const ProfileItem = ({ profile }) => (
-  <tr>
-    <td>{profile.firstName}</td>
-    <td>{profile.lastName}</td>
-    <td>{profile.age}</td>
-    <td>{profile.email}</td>
-    <td>
-      <Link to={`/profile/edit/${profile._id}`}>Edit</Link>
-    </td>
-  </tr>
+  <Container className="py-3">
+    <Col className="justify-content-end">
+      <thead>
+        <tr>
+          <th id="firstname" scope="col">
+            <tr>{profile.firstName}</tr>
+          </th>
+          <tr>{profile.lastName}</tr>
+          <tr>{profile.age}</tr>
+          <tr>{profile.email}</tr>
+          <tr>{profile.password}</tr>
+          <tr>{profile.bio}</tr>
+          <tr>
+            <Link to={`/profile/edit/${profile._id}`}>Edit</Link>
+          </tr>
+        </tr>
+      </thead>
+    </Col>
+  </Container>
 );
 
 // Require a document to be passed to this component.
@@ -23,6 +34,8 @@ ProfileItem.propTypes = {
     age: PropTypes.string,
     email: PropTypes.string,
     _id: PropTypes.string,
+    password: PropTypes.string,
+    bio: PropTypes.string,
   }).isRequired,
 };
 
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
-import { Col, Container, Row, Card } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Col, Container, Row } from 'react-bootstrap';
+// import { NavLink } from 'react-router-dom';
 import { Profiles } from '../../api/profile/Profiles';
-import ProfileItem from '../components/ProfileItem';
 import LoadingSpinner from '../components/LoadingSpinner';
+import ProfileItem from '../components/ProfileItem';
 
 /*
 Consider looking at this for account stuff: https://docs.meteor.com/api/accounts.html#Meteor-users
@@ -32,29 +32,57 @@ const Profile = () => {
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col className="text-center" />
-        <Card>
-          <Card.Body>
-            <div className="card-header"><h2>My Profile</h2>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">First Name </li>
-              <li className="list-group-item">Last Name</li>
-              <li className="list-group-item">Age</li>
-              <li className="list-group-item">Email Address</li>
-              <li className="list-group-item">About yourself</li>
-            </ul>
-            {/* <TextField name="firstName" placeholder="First Name" /> */}
-            {/* <TextField name="lastName" placeholder="Last Name" /> */}
-            {/* <TextField name="age" placeholder="Age" /> */}
-            {/* <TextField name="email" placeholder="Email" /> */}
-            {/* <TextField name="password" placeholder="Password" /> */}
-            {/* <LongTextField name="bio" placeholder="About Yourself" /> */}
+        <h2> My Profile</h2>
+        <table className="table table-sm">
+          <thead>
+            <tr>
+              <th scope="row">First Name:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th id="firstname" scope="col">Last Name:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+            <tr>
+              <th scope="col">Age:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+            <tr>
+              <th scope="row">Email:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+            <tr>
+              <th scope="row">Password:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+            <tr>
+              <th scope="row">About Yourself:</th>
+              <td> {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}</td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <Card> */}
+        {/*  <Card.Body> */}
+        {/*    <div className="card-header"><h2>My Profile</h2> */}
+        {/*    </div> */}
+        {/*    <ul className="list-group list-group-flush"> */}
+        {/*      <li className="list-group-item">First Name </li> */}
+        {/*      <li className="list-group-item">Last Name</li> */}
+        {/*      <li className="list-group-item">Age</li> */}
+        {/*      <li className="list-group-item">Email Address</li> */}
+        {/*      <li className="list-group-item">About yourself</li> */}
+        {/*    </ul> */}
+        {/*    /!* <TextField name="firstName" placeholder="First Name" /> *!/ */}
+        {/*    /!* <TextField name="lastName" placeholder="Last Name" /> *!/ */}
+        {/*    /!* <TextField name="age" placeholder="Age" /> *!/ */}
+        {/*    /!* <TextField name="email" placeholder="Email" /> *!/ */}
+        {/*    /!* <TextField name="password" placeholder="Password" /> *!/ */}
+        {/*    /!* <LongTextField name="bio" placeholder="About Yourself" /> *!/ */}
 
-          </Card.Body>
-        </Card>
-        <tbody>
-          {profiles.map((profile) => <ProfileItem key={profile._id} profile={profile} />)}
-        </tbody>
+        {/*  </Card.Body> */}
+        {/* </Card> */}
       </Row>
     </Container>
   ) : <LoadingSpinner />;
